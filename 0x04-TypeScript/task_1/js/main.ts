cdac1300fd402d2ea46b700f4fc398ee4f7192c4
Hagos Beyene <53543538+Hagos29@users.noreply.github.com>
2025-07-15 18:36:52 +0300

    build a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher {
+  firstName: String;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience: number;
+  location: string;
+  contract: boolean;
+}
+
+
+const teacher3: Teacher = {
+  firstName: 'Hagos',
+  fullTimeEmployee : false,
+  lastName: 'Beyene',
+  location: 'Eritrea',
+  contract: false,
+  yearsOfExperience: 2
+};
