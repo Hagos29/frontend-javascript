ca0ce32a9fe518ba97d840d7414acb71f09161a5
Hagos Beyene <53543538+Hagos29@users.noreply.github.com>
2025-07-15 19:43:15 +0300

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -39,3 +39,43 @@ function printTeacher
   }
 
   console.log(printTeacher("hagos", "Yacob"))
+
+
+  
+
+
+interface StudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+
+class StudentClass implements StudentInterface {
+   firstName: string;
+   lastName: string;
+
+  constructor({ firstName, lastName }: StudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+
+const student = new StudentClass({ firstName: 'Hagos', lastName: 'Beyene' });
+
+
+export default StudentClass;
